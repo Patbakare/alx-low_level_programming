2b7483dea99db0ad5080a827a12289fa76bcaf6a
Bakare Patience <Bakarepatience@yahoo.com>
2022-09-14 04:18:14 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,14 +2,13 @@
 
 /**
  * times_table -> times table function
- *
  */
 void times_table(void)
 {
 	int i;
 	int j;
 
-	for (i = 0: i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
